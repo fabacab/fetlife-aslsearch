261495ad95bf1466b5dd98c2cb79a03ab1862b8f
Meitar Moscovitz <meitarm@gmail.com>
2012-09-18 00:44:10 -0500

    Initial commit. Atomatic user search by age, sex, location, and role.

---
+++ README.markdown
@@ -0,0 +1,58 @@
+# FetLife Age/Sex/Location Search
+
+The FetLife Age/Sex/Location Search user script allows you to search for profiles on [FetLife](https://fetlife.com/) by age, sex, location, or orientation. This user script imlements what is, as of this writing, the [most popular suggestion in the FetLife suggestion box](https://fetlife.com/improvements/78):
+
+> Search for people by Location/Sex/Orientation/Age
+>
+> Increase the detail of the kinkster search by allowing us to narrow the definition of the search by the traditional fields.
+
+With the FetLife Age/Sex/Location Search user script installed, a few clicks will save hours of time. Now you can find profiles that match your specified criteria in a matter of seconds. The script even lets you send a message to the profiles you found right from the search results list.
+
+## System requirements
+
+To use the FetLife Age/Sex/Location Search user script, you must be running [Mozilla Firefox](http://getfirefox.com/) (version 12.0 or higher), with the [Greasemonkey extension](https://addons.mozilla.org/en-US/firefox/addon/greasemonkey/) installed (at version 0.9 or higher).
+
+Sorry, FetLife Age/Sex/Location Search is currently [incompatible with other Web browsers](https://github.com/meitar/fetlife-aslsearch/issues/1). (Please help me fix that!)
+
+## Installing
+
+To install FetLife Age/Sex/Location Search, go to [http://maybemaimed.com/playground/fetlife-aslsearch/](http://maybemaimed.com/playground/fetlife-aslsearch/) and click the "[Download and install](https://userscripts.org/scripts/source/146293.user.js)" near the middle of the page:
+
+> [Download and install FetLife Age/Sex/Location Search](https://userscripts.org/scripts/source/146293.user.js)
+
+If you enjoy this script, please consider tossing a few metaphorical coins in [my cyberbusking hat](http://maybemaimed.com/cyberbusking/). :) Your donations are sincerely appreciated! Can't afford to part with any coin? It's cool. [Tweet your appreciation, instead](https://twitter.com/intent/tweet?text=Ever%20wanted%20to%20search%20%23FetLife%20profiles%20by%20age%2Fsex%2Flocation%2Frole%3F%20Now%20we%20can%3A%20http%3A%2F%2Fmaybemaimed.com%2Fplayground%2Ffetlife-aslsearch%2F%20All%20thx%20to%20%40maymaym%3A%20http%3A%2F%2Fmaybemaimed.com%2Fcyberbusking%2F).
+
+If [maybemaimed.com is censored](http://maybemaimed.com/where-im-censored/) where you are, you can alternatively go to [the Userscripts.org page for FetLife Age/Sex/Location Search](https://userscripts.org/scripts/show/146293) and click on "[Install](http://userscripts.org/scripts/source/146293.user.js)".
+
+## Using
+
+To use FetLife Age/Sex/Location Search, [log in to your FetLife.com account](https://fetlife.com/login) and click the "A/S/L?" checkbox in the top navigation bar, near the Search box.
+
+You will be presented with a number of additional options that allow you to define your search criteria based on profile fields. Enter your desired search criteria, as shown.
+
+![Screenshot of form fields for FetLife Age/Sex/Location Search.](http://i.imgur.com/k6YEm.png)
+
+The image above shows selections that will search for the profile of any person whose gender/sex is Male or FtM, who is between the ages of 21 and 28 (inclusive), and who identifies as a submissive, slave, pet,  bottom, or masochist. Once you've made your selections, click the `Mine! I mean, uh, search…` button. A progress bar will replace the search button showing you how many pages have been searched for a match, as shown below:
+
+![Screenshot of FetLife Age/Sex/Location Search running a search.](http://i.imgur.com/zRBnN.png)
+
+When a match is found, it will automatically appear with a link allowing you to send a message to them. The progress bar will again be replaced with a button allowing you to continue the search. This is shown in the image below:
+
+![Screenshot of FetLife Age/Sex/Location Search having found a match.](http://i.imgur.com/0p66t.png)
+
+To start a new search, simply reload the page.
+
+## Frequently Asked Questions
+
+Before you report a new issue with FetLife Age/Sex/Location Search, please check to ensure your question is not already addressed in the list below.
+
+* [Can I search in a location other than "near me"?](#can-i-search-in-a-location-other-than-near-me)
+* [Why can I only search for men, not women?](#why-can-i-only-search-for-men-not-women)
+
+### Can I search in a location other than "near me"?
+
+The current version of FetLife Age/Sex/Location Search doesn't provide an interface for you to choose a location directly. However, you can temporarily change the location you list on your own profile to search in different regions. In other words, if you're currently in New York City but want to search for people in Tokyo, temporarily change your FetLife profile's location to Tokyo and run your search again. Afterwards, you can change your FetLife profile's location back to New York City.
+
+### Why can I only search for men, not women?
+
+Because fuck you [that's why](http://www.notjustbitchy.com/?p=496).
---
+++ fetlife-age-sex-location-search.user.js
@@ -0,0 +1,400 @@
+/**
+ *
+ * This is a Greasemonkey script and must be run using Greasemonkey 0.8 or newer, or Google Chrome.
+ *
+ * @author maymay <bitetheappleback@gmail.com>
+ */
+// ==UserScript==
+// @name           FetLife ASL Search
+// @version        0.1
+// @namespace      http://maybemaimed.com/playground/fetlife-aslsearch/
+// @updateURL      https://userscripts.org/scripts/source/146293.user.js
+// @description    Allows you to search for FetLife profiles based on age, sex, location, and role.
+// @include        https://fetlife.com/administrative_areas*
+// @include        https://fetlife.com/cities*
+// @include        https://fetlife.com/countries*
+// @include        https://fetlife.com/events*
+// @include        https://fetlife.com/fetishes*
+// @include        https://fetlife.com/fetlife*
+// @include        https://fetlife.com/groups*
+// @include        https://fetlife.com/home*
+// @include        https://fetlife.com/improvements*
+// @include        https://fetlife.com/places*
+// @include        https://fetlife.com/posts*
+// @include        https://fetlife.com/search*
+// @include        https://fetlife.com/users*
+// @include        https://fetlife.com/videos*
+// @exclude        https://fetlife.com/adgear/*
+// @exclude        https://fetlife.com/chat/*
+// @exclude        https://fetlife.com/im_sessions*
+// @exclude        https://fetlife.com/polling/*
+// ==/UserScript==
+
+FL_ASL = {};
+FL_ASL.CONFIG = {
+    'debug': false, // switch to true to debug.
+    'progress_id': 'fetlife_asl_search_progress'
+};
+
+// Utility debugging function.
+FL_ASL.log = function (msg) {
+    if (!FL_ASL.CONFIG.debug) { return; }
+    GM_log('FETLIFE ASL SEARCH: ' + msg);
+};
+
+// Initializations.
+var uw = (unsafeWindow) ? unsafeWindow : window ; // Help with Chrome compatibility?
+GM_addStyle('\
+#fetlife_asl_search_options { display: none; }\
+#fetlife_asl_search_options fieldset { clear: both; margin: 0; padding: 0; }\
+#fetlife_asl_search_options legend { display: none; }\
+#fetlife_asl_search_options label {\
+    display: inline-block;\
+    white-space: nowrap;\
+}\
+#fetlife_asl_search_options input { width: auto; }\
+#fetlife_asl_search_results { clear: both; }\
+');
+FL_ASL.users = {};
+FL_ASL.init = function () {
+    FL_ASL.CONFIG.search_form = document.querySelector('form[action="/search"]').parentNode;
+    FL_ASL.getUserProfile(uw.FetLife.currentUser.id);
+    FL_ASL.main();
+};
+window.addEventListener('DOMContentLoaded', FL_ASL.init);
+
+FL_ASL.toggleAslSearch = function () {
+    var el = document.getElementById('fetlife_asl_search_options');
+    if (el.style.display == 'block') {
+        el.style.display = 'none';
+    } else {
+        el.style.display = 'block';
+    }
+};
+
+FL_ASL.aslSubmit = function (e) {
+    var el = document.getElementById('fetlife_asl_search');
+    if (!el.checked) {
+        return false;
+    }
+
+    // Provide UI feedback.
+    var prog = document.getElementById(FL_ASL.CONFIG.progress_id);
+    prog.innerHTML = 'Searching&hellip;<br />';
+
+    // collect the form parameters
+    var search_params = FL_ASL.getSearchParams();
+
+    // search one of the geographic regions "/kinksters" list
+    FL_ASL.getKinkstersInLocation(search_params.loc);
+
+    return false;
+};
+
+/**
+ * Reads and saves the search parameters from the provided form.
+ */
+FL_ASL.getSearchParams = function () {
+    var r = {
+        'age'  : {'min': null, 'max': null},
+        'sex'  : [],
+        'role' : [],
+        'loc'  : null
+    };
+
+    // Collect age parameters, setting wide defaults.
+    r.age.min = (document.getElementById('min_age').value) ? parseInt(document.getElementById('min_age').value) : 1;
+    r.age.max = (document.getElementById('max_age').value) ? parseInt(document.getElementById('max_age').value) : 99;
+
+    // Collect gender/sex parameters.
+    var x = FL_ASL.CONFIG.search_form.querySelectorAll('input[name="user[sex]"]');
+    for (var i = 0; i < x.length; i++) {
+        if (x[i].checked) {
+            r.sex.push(x[i].value);
+        }
+    }
+
+    // Collect role orientation parameters.
+    var y = FL_ASL.CONFIG.search_form.querySelectorAll('input[name="user[role]"]');
+    for (var iy = 0; iy < y.length; iy++) {
+        if (y[iy].checked) {
+            r.role.push(y[iy].value);
+        }
+    }
+
+    // Collect location parameters.
+    // TODO: Create an interface for setting locations other than closest-to-me.
+    r.loc = FL_ASL.getLocationForUser(uw.FetLife.currentUser.id);
+
+    return r;
+};
+
+FL_ASL.getLocationForUser = function (id) {
+    var r = {
+        'city_id': null,
+        'area_id': null,
+        'country': null
+    };
+    var profile_html = FL_ASL.users[id].profile_html;
+    var m = profile_html.match(/href="\/countries\/([0-9]+)/);
+    if (m) {
+        r.country = m[1];
+    }
+    m = profile_html.match(/href="\/administrative_areas\/([0-9]+)/);
+    if (m) {
+        r.area_id = m[1];
+    }
+    m = profile_html.match(/href="\/cities\/([0-9]+)/);
+    if (m) {
+        r.city_id = m[1];
+    }
+
+    return r;
+};
+
+FL_ASL.getUserProfile = function (id) {
+    if (FL_ASL.users[id]) {
+        return FL_ASL.users[id].profile_html;
+    } else {
+        FL_ASL.users[id] = {};
+        GM_xmlhttpRequest({
+            'method': "GET",
+            'url': 'https://fetlife.com/users/' + id.toString(),
+            'onload': function (response) {
+                FL_ASL.users[id].profile_html = response.responseText;
+            }
+        });
+    }
+};
+
+FL_ASL.getKinkstersInLocation = function (loc_obj) {
+    if (loc_obj.city_id) {
+        FL_ASL.getKinkstersInCity(loc_obj.city_id);
+    } else if (loc_obj.area_id) {
+        FL_ASL.getKinkstersInArea(loc_obj.area_id);
+    } else if (loc_obj.country) {
+        FL_ASL.getKinkstersInCountry(loc_obj.country);
+    } else {
+        return false;
+    }
+};
+
+FL_ASL.getKinkstersInCity = function (city_id, page) {
+    var url = 'https://fetlife.com/cities/' + city_id.toString() + '/kinksters';
+    url = (page) ? url + '?page=' + page.toString() : url ;
+    FL_ASL.getKinkstersFromURL(url);
+};
+FL_ASL.getKinkstersInArea = function (area_id, page) {
+    var url = 'https://fetlife.com/administrative_areas/' + area_id.toString() + '/kinksters';
+    url = (page) ? url + '?page=' + page.toString() : url ;
+    FL_ASL.getKinkstersFromURL(url);
+};
+FL_ASL.getKinkstersInCountry = function (country, page) {
+    var url = 'https://fetlife.com/countries/' + country.toString() + '/kinksters';
+    url = (page) ? url + '?page=' + page.toString() : url ;
+    FL_ASL.getKinkstersFromURL(url);
+};
+FL_ASL.getKinkstersFromURL = function (url) {
+    FL_ASL.log('Getting Kinksters list from URL: ' + url);
+    prog = document.getElementById(FL_ASL.CONFIG.progress_id);
+    prog.innerHTML = prog.innerHTML + '.';
+    GM_xmlhttpRequest({
+        'method': 'GET',
+        'url': url,
+        'onload': function (response) {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(response.responseText, 'text/html');
+            var els = doc.querySelectorAll('.user_in_list');
+
+            result_count = 0;
+            for (var i = 0; i < els.length; i++) {
+                // filter the results based on the form parameters
+                if (FL_ASL.matchesSearchParams(els[i])) {
+                    // display the results in a "results" section in this portion of the page
+                    FL_ASL.displayResult(els[i]);
+                    result_count++;
+                }
+            }
+
+            // Set up next request.
+            my_page = (url.match(/\d+$/)) ? parseInt(url.match(/\d+$/)[0]) : 1 ;
+            next_page = my_page + 1;
+            if (next_page > 2) {
+                next_url = url.replace(/\d+$/, next_page.toString());
+            } else {
+                next_url = url + '?page=' + next_page.toString();
+            }
+
+            // Automatically search on next page if no results found.
+            if (0 === result_count) {
+                FL_ASL.getKinkstersFromURL(next_url);
+                return false;
+            } else {
+                // Reset UI search feedback.
+                p = prog.parentNode
+                p.removeChild(prog);
+                new_prog = document.createElement('p');
+                new_prog.setAttribute('id', FL_ASL.CONFIG.progress_id);
+                p.appendChild(new_prog);
+            }
+            btn = document.createElement('button');
+            btn.setAttribute('id', 'btn_moar');
+            btn.setAttribute('onclick', "var xme = document.getElementById('btn_moar'); xme.parentNode.removeChild(xme); return false;");
+            btn.innerHTML = 'Show me MOAR&hellip;';
+            btn.addEventListener('click', function(){FL_ASL.getKinkstersFromURL(next_url)});
+            document.getElementById('fetlife_asl_search_results').appendChild(btn);
+        }
+    });
+};
+
+/**
+ * Determines whether a "user_in_list" block matches the searched-for parameters.
+ *
+ * @return True if block matches all search parameters, false otherwise.
+ */
+FL_ASL.matchesSearchParams = function (el) {
+    var search_params = FL_ASL.getSearchParams();
+
+    // Does block match age range?
+    age = FL_ASL.getAge(el);
+    // Did we supply a minimum age?
+    if (search_params.age.min && (search_params.age.min > age) ) {
+        return false;
+    }
+    // Did we supply a maximum age?
+    if (search_params.age.max && (search_params.age.max < age) ) {
+        return false;
+    }
+
+    // Does block match gender/sex selection?
+    if (-1 === search_params.sex.indexOf(FL_ASL.getSex(el))) {
+        return false;
+    }
+
+    // Does block match role orientation selection?
+    if (-1 === search_params.role.indexOf(FL_ASL.getRole(el))) {
+        return false;
+    }
+
+    // All conditions match.
+    return true;
+};
+
+FL_ASL.getSex = function (el) {
+    x = el.querySelector('.quiet').innerHTML;
+    sex = x.match(/^\d\d(\S*)/);
+    return sex[1];
+};
+
+FL_ASL.getAge = function (el) {
+    x = el.querySelector('.quiet').innerHTML;
+    age = x.match(/^\d\d/);
+    return parseInt(age);
+};
+
+FL_ASL.getRole = function (el) {
+    x = el.querySelector('.quiet').innerHTML;
+    role = x.match(/ ?(\S+)?$/);
+    return role[1];
+};
+
+FL_ASL.displayResult = function (el) {
+    var id = el.querySelector('a').href.match(/\d+$/);
+    var name = el.querySelector('.large a').childNodes[0].nodeValue;
+    var a = document.createElement('a');
+    a.href = 'https://fetlife.com/conversations/new?with=' + id;
+    a.innerHTML = '(send ' + name + ' a message)';
+    a.style.textDecoration = 'underline';
+    a.setAttribute('target', '_blank');
+    el.appendChild(a);
+    document.getElementById('fetlife_asl_search_results').appendChild(el);
+};
+
+// This is the main() function, executed on page load.
+FL_ASL.main = function () {
+    // Insert ASL search button interface at FetLife "Search" bar.
+    var label = document.createElement('label');
+    label.innerHTML = 'A/S/L?';
+    var input = document.createElement('input');
+    input.setAttribute('style', '-webkit-appearance: checkbox');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('id', 'fetlife_asl_search');
+    input.setAttribute('name', 'fetlife_asl_search');
+    input.setAttribute('value', '1');
+    input.addEventListener('click', FL_ASL.toggleAslSearch);
+    label.appendChild(input);
+    var div = document.createElement('div');
+    div.setAttribute('id', 'fetlife_asl_search_options');
+    div.setAttribute('style', 'display: none;');
+    html_string = '<fieldset><legend>Search for user profiles of the following gender/sex:</legend><p>';
+    html_string += 'Show me profiles of people with a gender/sex of&hellip;';
+    // NOTE: What if the UI only allowed us to find male-ish identified people, not women? What would the response be? :)
+    html_string += '<label><input type="checkbox" name="user[sex]" value="M" checked="checked" /> Male</label>';
+//    html_string += '<label><input type="checkbox" name="user[sex]" value="F" /> Female</label>';
+//    html_string += '<label><input type="checkbox" name="user[sex]" value="CD/TV" />Crossdresser/Transvestite</label>';
+    html_string += '<label><input type="checkbox" name="user[sex]" value="MtF" />Trans - Male to Female</label>';
+    html_string += '<label><input type="checkbox" name="user[sex]" value="FtM" checked="checked" />Trans - Female to Male</label>';
+    html_string += '<label><input type="checkbox" name="user[sex]" value="TG" />Transgender</label>';
+    html_string += '<label><input type="checkbox" name="user[sex]" value="GF" />Gender Fluid</label>';
+    html_string += '<label><input type="checkbox" name="user[sex]" value="GQ" />Genderqueer</label>';
+    html_string += '<label><input type="checkbox" name="user[sex]" value="IS" />Intersex</label>';
+    html_string += '<label><input type="checkbox" name="user[sex]" value="B" />Butch</label>';
+//    html_string += '<label><input type="checkbox" name="user[sex]" value="FEM" />Femme</label>';
+    html_string += '</p></fieldset>';
+    html_string += '<fieldset><legend>Search for user profiles between the ages of:</legend><p>';
+    html_string += '&hellip;who are also <label>at least <input type="text" name="min_age" id="min_age" size="2" /> years old</label> and <label>at most <input type="text" name="max_age" id="max_age" size="2" /> years old&hellip;</label>';
+    html_string += '</p></fieldset>';
+    html_string += '<fieldset><legend>Search for user profiles whose role is:</legend><p>';
+    html_string += '&hellip;who identify their role as ';
+    // Note that these values are what show up, not necessarily what's sent to the FetLife backend.
+    html_string += '<label><input type="checkbox" name="user[role]" value="Dom" />Dominant</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Domme" />Domme</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Switch" />Switch</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="sub" checked="checked" />submissive</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Master" />Master</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Mistress" />Mistress</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="slave" checked="checked" />slave</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="pet" checked="checked" />pet</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="kajira" />kajira</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="kajirus" />kajirus</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Top" />Top</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="bottom" checked="checked" />Bottom</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Sadist" />Sadist</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Masochist" checked="checked" />Masochist</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Sadomasochist" />Sadomasochist</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Ageplayer" />Ageplayer</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Daddy" />Daddy</label>';	
+    html_string += '<label><input type="checkbox" name="user[role]" value="babygirl" />babygirl</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="brat" />brat</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Primal" />Primal</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Fetishist" />Fetishist</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Kinkster" />Kinkster</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Hedonist" />Hedonist</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Vanilla" />Vanilla</label>';
+    html_string += '<label><input type="checkbox" name="user[role]" value="Unsure" />Unsure</label>';
+    // Note that "Not Applicable" is the equivalent of "it doesn't matter", so we omit this.
+    //html_string += '<label><input type="checkbox" name="user[role]" value="" />Not Applicable</label>';
+    html_string += '</p></fieldset>';
+    html_string += '<fieldset><legend>Search for user profiles located in:</legend><p>';
+    // TODO: Change "near me" to options offering "my city," "my region," and "my country."
+    html_string += '&hellip;located near me.';
+    html_string += '</p></fieldset>';
+    div.innerHTML = html_string;
+    FL_ASL.CONFIG.search_form.appendChild(label);
+    FL_ASL.CONFIG.search_form.appendChild(div);
+
+    btn_submit = document.createElement('button');
+    btn_submit.setAttribute('id', 'btn_fetlife_asl_search_submit');
+    btn_submit.setAttribute('onclick', "var xme = document.getElementById('btn_fetlife_asl_search_submit'); xme.parentNode.removeChild(xme); return false;");
+    btn_submit.innerHTML = 'Mine! (I mean, uh, search&hellip;)';
+    btn_submit.addEventListener('click', FL_ASL.aslSubmit);
+    div.appendChild(btn_submit);
+
+    results_container = document.createElement('div');
+    results_container.setAttribute('id', 'fetlife_asl_search_results');
+    FL_ASL.CONFIG.search_form.appendChild(results_container);
+
+    prog = document.createElement('p');
+    prog.setAttribute('id', FL_ASL.CONFIG.progress_id);
+    FL_ASL.CONFIG.search_form.appendChild(prog);
+};
