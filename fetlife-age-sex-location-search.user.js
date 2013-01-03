7ecd28e451d3fd18b1aae21f77a00c3e25ee67aa
Meitar Moscovitz <meitarm@gmail.com>
2013-01-02 16:02:56 -0800

    Merge FAADE v0.2 to FetLife ASL Search v0.3.6.
    This commit merges FAADE commit 3d99b58dcf02551fa0fe09197aeb9e705615768e
    into FetLife ASL Search. Both FAADE and ASL Search share some functions.

--- fetlife-age-sex-location-search.user.js
+++ fetlife-age-sex-location-search.user.js
@@ -21,6 +21,7 @@
 // @grant          GM_getValue
 // @grant          GM_setValue
 // @grant          GM_deleteValue
+// @grant          GM_openInTab
 // ==/UserScript==
 
 FL_ASL = {};
@@ -530,6 +531,7 @@ FAADE.log = function (msg) {
 };
 
 // Initializations.
+var uw = (unsafeWindow) ? unsafeWindow : window ; // Help with Chrome compatibility?
 GM_addStyle('\
 /* Highlight alleged abusers in bright yellow. */\
 .faade_alleged_abuser {\
@@ -544,6 +546,18 @@ GM_addStyle('\
     padding-top: 1em;\
     text-align: center;\
 }\
+#faade_abuse_reports tr:target > * {\
+    border: 1px solid red;\
+}\
+#faade_abuse_reports tr:target th {\
+    border-width: 1px 0 1px 1px;\
+}\
+#faade_abuse_reports tr:target td {\
+    border-width: 1px 1px 1px 0;\
+}\
+/* FAADE location broadcast dialog styles. */\
+[aria-labelledby="ui-dialog-title-faade"] { border-color: yellow; }\
+#ui-dialog-title-faade { color: red; }\
 /* General prettiness. */\
 #profile #main_content a + a.faade_report_link { padding-left: 5px; }\
 footer .faade_report_link,\
@@ -560,6 +574,8 @@ ul.pictures li a.faade_report_link,\
 #profile ul.friends li { width: auto; }\
 ');
 FAADE.init = function () {
+    FL_ASL.getUserProfile(uw.FetLife.currentUser.id); // run early
+    FAADE.injectDialog();
     FAADE.abuser_database = FAADE.getValue('abuser_database', false);
     if (FAADE.abuserDatabaseExpired()) {
         FAADE.fetchAbuserDatabase();
@@ -631,17 +647,118 @@ FAADE.fetchAbuserDatabase = function () {
     });
 };
 
+FAADE.injectDialog = function () {
+    // Inject hidden dialog box link.
+    var trigger_el = document.createElement('a');
+    trigger_el.setAttribute('class', 'opens-modal');
+    trigger_el.setAttribute('data-opens-modal', 'faade');
+    document.body.appendChild(trigger_el);
 
-FAADE.createAbuseReportLink = function (id, nick) {
-    var a = document.createElement('a');
-    a.setAttribute('class', 'faade_report_link');
-    a.setAttribute('target', '_blank');
-    var href = 'https://docs.google.com/spreadsheet/viewform?formkey=' + FAADE.getReportFormKey();
-    href += '&entry_0=' + id;
-    href += '&entry_1=' + nick;
-    a.setAttribute('href', href);
-    a.innerHTML = '(report a consent violation by ' + nick + ')';
-    return a;
+    // Inject dialog box HTML. FetLife currently uses Rails 3, so mimic that.
+    // See, for instance, Rails Behaviors: http://josh.github.com/rails-behaviors/
+    var faade_dialog = document.createElement('div');
+    faade_dialog.setAttribute('style', 'display: none; position: absolute; overflow: hidden; z-index: 1000; outline: 0px none;');
+    faade_dialog.setAttribute('class', 'ui-dialog ui-widget ui-widget-content ui-corner-all');
+    faade_dialog.setAttribute('tabindex', '-1');
+    faade_dialog.setAttribute('role', 'dialog');
+    faade_dialog.setAttribute('aria-labelledby', 'ui-dialog-title-faade');
+    var html_string = '<div class="ui-dialog-titlebar ui-widget-header ui-corner-all ui-helper-clearfix" unselectable="on" style="-moz-user-select: none;">';
+    html_string += '<span class="ui-dialog-title" id="ui-dialog-title-faade" unselectable="on" style="-moz-user-select: none;">FetLife Alleged Abusers Database Engine (FAADE)</span>';
+    html_string += '<a href="#" class="ui-dialog-titlebar-close ui-corner-all" role="button" unselectable="on" style="-moz-user-select: none;">';
+    html_string += '<span class="ui-icon ui-icon-closethick" unselectable="on" style="-moz-user-select: none;">close</span>';
+    html_string += '</a>';
+    html_string += '</div>';
+    html_string += '<div data-modal-title="FetLife Alleged Abusers Database Engine (FAADE)" data-modal-height="280" data-modal-auto-open="false" class="modal ui-dialog-content ui-widget-content" id="faade">';
+    html_string += '<p class="mbm">There have been <span id="faade_reports_to_alert">X</span> new consent violations filed in FAADE that may have been perpetrated near your location (<span id="faade_user_loc">X, X, X</span>).</p>';
+    html_string += '<p>Click "View new nearby FAADE reports" to view the profiles of the people who have been accused of consent violations near your area in new tabs.</p>';
+    html_string += '<p id="faade-actions" class="ac">';
+    html_string += '<a rel="nofollow" class="btnsqr close" data-closes-modal="faade" href="#">View new nearby FAADE reports</a>';
+    html_string += '<span class="i s q">&nbsp;-or-&nbsp;</span>';
+    html_string += '<a data-closes-modal="faade" class="close tdn q" href="#">Cancel</a>';
+    html_string += '</p>';
+    html_string += '<p>(Don\'t worry, I\'m not looking for where you actually are. Your location was determined from your FetLife profile.)</p>';
+    html_string += '</div>';
+    faade_dialog.innerHTML = html_string;
+    document.body.appendChild(faade_dialog);
+
+    // Attach event listener to trigger element.
+    document.querySelector('[data-opens-modal="faade"]').addEventListener('click', function (e) {
+        document.querySelector('[data-opens-modal="faade"]').dialog("open");
+    });
+};
+
+FAADE.getLocationFromProfileHtml = function (html) {
+    var parser = new DOMParser();
+    var doc = parser.parseFromString(html, 'text/html');
+    return doc.querySelector('h2.bottom + p > em').textContent.split(', '); // split with comma AND space
+};
+
+FAADE.broadcastNewProximalReports = function (doc) {
+    // Recall timestamp of last record checked.
+    var last_timestamp_checked = parseInt(FAADE.getValue('last_timestamp_checked', '0')); // default is "never!"
+    // Get latest timestamp in stored alleged abuser database.
+    var rows = doc.querySelectorAll('#tblMain tr'); // read in every report, in full
+    var latest_timestamp_filed = Date.parse(rows[rows.length - 1].childNodes[1].textContent);
+
+    // If never checked, or if there are new records since last timestamp checked
+    if (last_timestamp_checked < latest_timestamp_filed) {
+        FAADE.log('Last timestamp checked (' + last_timestamp_checked.toString() + ') is older than latest timestamp filed (' + latest_timestamp_filed.toString() + ').');
+
+        // count how many new records there are since last check
+        var num_reports = 0;
+        for (var i = rows.length - 1; i > 0; i--) {
+            if (Date.parse(rows[i].childNodes[1].textContent) > last_timestamp_checked) {
+                num_reports++;
+            } else {
+                break; // we've reached the timestamps we've already checked, we're done
+            }
+        }
+        FAADE.log('Total of ' + num_reports + ' new reports since last check.');
+
+        var user_loc = FAADE.getLocationFromProfileHtml(FL_ASL.users[uw.FetLife.currentUser.id].profile_html);
+        FAADE.log('Current user location seems to be ' + user_loc.join(', ') + '.');
+
+        // Loop over all new records one by one
+        var reports_to_alert = [];
+        for (var i = rows.length - num_reports; i <= rows.length - 1; i++) {
+            // extract the location data from the report
+            report_loc = rows[i].childNodes[6].textContent;
+            // compare current user's FetLife profile location against alleged abuse location
+            // and test each substring of the user profile against the reported location
+            for (var z = 0; z < user_loc.length; z++) {
+                // if a relevant case insensitive substring matches
+                if (-1 !== report_loc.toLowerCase().search(user_loc[z].toLowerCase())) {
+                    FAADE.log('Filed report location ' + report_loc + ' matches user location substring ' + user_loc[z] + '!');
+                    // store for future pop-up
+                    reports_to_alert.push(rows[i]);
+                    break; // we found a match, so stop trying on this row
+                }
+            }
+        }
+
+        // Ask user to view the profiles of the alleged abusers in the user's local vicinity.
+        if (reports_to_alert.length) {
+            // Fill in the user-facing message with the appropriate info.
+            document.getElementById('faade_reports_to_alert').innerHTML = reports_to_alert.length.toString();
+            document.getElementById('faade_user_loc').innerHTML = user_loc.join(', ');
+            // Create the click event we're going to use.
+            var evt = document.createEvent('MouseEvents');
+            evt.initEvent('click', true, false); // can bubble, can't be cancelled
+            // "Click" event on hidden code.
+            document.querySelector('a[data-opens-modal="faade"]').dispatchEvent(evt);
+            // Attach event listener to "View" button and pass in appropriate URLs.
+            document.querySelector('.btnsqr[data-closes-modal="faade"]').addEventListener('click', function () {
+                for (var i = 0; i < reports_to_alert.length; i++) {
+                    // TODO: Add the permalink to the specific report to this URL, so it's highlighted when opened.
+                    var url = 'https://fetlife.com/users/';
+                    GM_openInTab(url + reports_to_alert[i].childNodes[2].textContent.match(/\d+/)[0]);
+                }
+            });
+        }
+    }
+
+    // Make a note of the latest timestamp filed, so we start here next time we're loaded.
+    FAADE.setValue('last_timestamp_checked', latest_timestamp_filed.toString());
 };
 
 // This is the main() function, executed on page load.
@@ -655,6 +772,15 @@ FAADE.main = function () {
         abuser_ids.push(els[i].innerHTML);
     }
     FAADE.log('recalled abuser ids ' + abuser_ids);
+
+    // TODO: Refactor this, it's kludgy.
+    setTimeout(function() {
+        FAADE.log('Running time-delayed function.');
+        if (FL_ASL.users[uw.FetLife.currentUser.id].profile_html) {
+            FAADE.log('We have the current user\'s FetLife profile HTML. Running broadcast checks.');
+            FAADE.broadcastNewProximalReports(doc);
+        }
+    }, 5000); // give us a few seconds to grab the current user's FetLife profile HTML.
 
     // Are we on a user profile page?
     if (window.location.href.match(/users\/(\d+)\/?$/)) {
@@ -694,9 +820,14 @@ FAADE.main = function () {
             for (var iy = 0; iy < abuse_reports.length; iy++) {
                 var num = iy + 1;
                 var tr = document.createElement('tr');
-                details_html = '<ul><li class="faade_abuse_report_datetime">' + abuse_reports[iy].childNodes[7].innerHTML + '</li>';
+                tr.setAttribute('id', 'faade_abuse_report-' + num.toString());
+                var details_html = '<ul><li class="faade_abuse_report_datetime">' + abuse_reports[iy].childNodes[7].innerHTML + '</li>';
                 details_html += '<li class="faade_abuse_report_location">' + abuse_reports[iy].childNodes[6].innerHTML + '</li></ul>';
-                tr.innerHTML += '<th>Abuse report ' + num.toString() + ' (<span class="faade_abuse_reported_datetime">' +  abuse_reports[iy].childNodes[1].innerHTML + '</span>):' + details_html + '</th>';
+                var permalink_html = '<a class="faade_abuse_reported_datetime" rel="bookmark" href="'
+                    + window.location + '#faade_abuse_report-' + num.toString()
+                    + '" title="Permalink for FAADE abuse report number ' + num.toString() + ' against '
+                    + profile_nick + '.">' +  abuse_reports[iy].childNodes[1].innerHTML + '</a>';
+                tr.innerHTML += '<th>Abuse report ' + num.toString() + ' (' + permalink_html + '):' + details_html + '</th>';
                 tr.innerHTML += '<td>' + abuse_reports[iy].childNodes[5].innerHTML + '</td>';
                 report_el.appendChild(tr);
             }
@@ -751,6 +882,18 @@ FAADE.main = function () {
         user_links[i].parentNode.appendChild(a);
         last_id = id;
     }
+};
+
+FAADE.createAbuseReportLink = function (id, nick) {
+    var a = document.createElement('a');
+    a.setAttribute('class', 'faade_report_link');
+    a.setAttribute('target', '_blank');
+    var href = 'https://docs.google.com/spreadsheet/viewform?formkey=' + FAADE.getReportFormKey();
+    href += '&entry_0=' + id;
+    href += '&entry_1=' + nick;
+    a.setAttribute('href', href);
+    a.innerHTML = '(report a consent violation by ' + nick + ')';
+    return a;
 };
 
 // The following is required for Chrome compatibility, as we need "text/html" parsing.
